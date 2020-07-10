c53fc3815b3fe2228f4b1969414bdeb17a803b6a
varunbhaya03 <varun.bhaya03@gmail.com>
2020-07-10 18:31:00 +0530

    environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,60 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
-  },
-  devServer: {
-    open: true,
-    port: 3000,
-    stats:'errors-only'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude:/node_modules/,
-        use:'babel-loader'
-      }, {
-        test: /\.s?[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }, {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use:'file-loader'
-      }, {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader',
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack app',
-      filename: 'index.html',
-      template: './public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    new ErrorOverlayPlugin()
-  ],
-  devtool: 'cheap-module-source-map'
+const config = (env) => {
+
+  const isDevelopment = env === 'development'
+  const fontPath = isDevelopment ? '[name].[ext]' : '/static/fonts/[name].[ext]'
+  const imagePath = isDevelopment ? '[name].[ext]' : '/static/images/[name].[hash].[ext]'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.[hash].js'
+    },
+    devServer: {
+      open: true,
+      port: 3000,
+      stats:'errors-only'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude:/node_modules/,
+          use:'babel-loader'
+        }, {
+          test: /\.s?[ac]ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        }, {
+          test: /\.(png|jpe?g|gif|svg)$/,
+          loader:'file-loader',
+          options: {
+            name: imagePath
+          }
+        }, {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          loader: 'file-loader',
+          options: {
+            name:fontPath,
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack app',
+        filename: 'index.html',
+        template: './public/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+      new ErrorOverlayPlugin()
+    ],
+    devtool: 'cheap-module-source-map'
+  }
 }
 
 module.exports = config
